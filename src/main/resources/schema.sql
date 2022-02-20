57dfdc35696b1955adbe42627a8c9cc8433adf8a
uradatafarm <ura@datafarm.co.th>
2022-02-20 20:21:56 +0700

    [ECOM-02] Add user data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,16 @@ CREATE TABLE product (
     name VARCHAR(50) NOT NULL,
     price INT NOT NULL
 );
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    userid VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    houseid VARCHAR(50) NOT NULL,
+    road VARCHAR(50) NOT NULL,
+    province VARCHAR(50) NOT NULL,
+    postid VARCHAR(50) NOT NULL
+);
